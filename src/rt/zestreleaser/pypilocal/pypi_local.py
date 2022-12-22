be190cd4479975722c867b92c3e4cf61ce7f5cd1
Andrea Cecchi <andrea.cecchi85@gmail.com>
2022-12-22 11:50:57 +0100

    python3 support

--- src/rt/zestreleaser/pypilocal/pypi_local.py
+++ src/rt/zestreleaser/pypilocal/pypi_local.py
@@ -4,34 +4,40 @@ import os
 import os.path
 import shutil
 import glob
-import ConfigParser
 import zest.releaser.utils
+
+try:
+    import ConfigParser as configparser
+except ModuleNotFoundError:
+    # python3
+    import configparser
+
 
 def copy(context):
     destinations = get_destinations(
-        context['name'], read_configuration('~/.pypirc'),
-        'rt.zestreleaser.pypilocal')
+        context["name"], read_configuration("~/.pypirc"), "rt.zestreleaser.pypilocal"
+    )
     if not destinations:
         return
 
     cwd = os.getcwd()
     for destination in destinations:
         target = None
-        if os.path.isdir(destination) and not destination.startswith('.'):
+        if os.path.isdir(destination) and not destination.startswith("."):
             target = destination
         elif os.path.isdir(os.path.join(cwd, destination)):
             target = os.path.abspath(os.path.join(cwd, destination))
-    
-        if not target or not zest.releaser.utils.ask('Copy egg to folder %s' % target):
+
+        if not target or not zest.releaser.utils.ask("Copy egg to folder %s" % target):
             continue
-    
-        sources = glob.glob(os.path.join(context['tagdir'], 'dist', '*'))
+
+        sources = glob.glob(os.path.join(context["tagdir"], "dist", "*"))
         for source in sources:
             shutil.copy(source, target)
 
 
 def read_configuration(filename):
-    config = ConfigParser.ConfigParser()
+    config = configparser.ConfigParser()
     config.read(os.path.expanduser(filename))
     return config
 
